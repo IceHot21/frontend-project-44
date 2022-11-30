16386e49c024f35a91878113d912860ba849fbc1
Ulquiorra <icehot21@mail.ru>
2022-11-30 19:48:38 +0300

    calc, even,gcl,progression

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,10 +4,13 @@ import readlineSync from 'readline-sync';
 import getEven from './brain-even.js';
 import getCalc from './brain-calc.js';
 import getGcd from './brain-gcd.js';
+import getProgression from './brain-progression.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name?');
 console.log('Hello,', name, '!');
-getEven(name);
-getCalc(name);
-getGcd(name)
+// getEven(name);
+// getCalc(name);
+// getGcd(name);
+getProgression(name);
+
