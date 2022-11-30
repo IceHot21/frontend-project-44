0800742827bfa57c7a3748d30d5eb28746b555cf
Ulquiorra <icehot21@mail.ru>
2022-11-30 22:14:00 +0300

    progression export

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -5,12 +5,13 @@ import getEven from './brain-even.js';
 import getCalc from './brain-calc.js';
 import getGcd from './brain-gcd.js';
 import getProgression from './brain-progression.js';
+import getPrime from './brain-prime.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name?');
 console.log('Hello,', name, '!');
-// getEven(name);
-// getCalc(name);
-// getGcd(name);
+getEven(name);
+getCalc(name);
+getGcd(name);
 getProgression(name);
-
+getPrime(name)
---
+++ bin/brain-prime.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import getRandomArbitrary from './random.number.js';
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+export default function getPrime(name) {
+  let i = 0;
+  while (i < 3) {
+    const numberRandom = getRandomArbitrary(2, 50);
+    console.log('Question: ', numberRandom);
+    const answ = readlineSync.question('Your answer: ');
+    if (numberRandom <= 5 && numberRandom !== 4 && answ === 'yes') {
+      i += 1;
+      console.log('Correct!');
+    } else if (numberRandom % 2 !== 0 && numberRandom % 3 !== 0 && numberRandom % 5 !== 0 && answ === 'yes') {
+      i += 1;
+      console.log('Correct!');
+    } else if (answ === 'no') {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      console.log("It is wrong answer ;(. Let's try again, ", name);
+      break;
+    }
+  }
+  if (i === 3) {
+    console.log('Congratulations, ', name);
+  }
+}
